d08b4cfebf42b45a5d7a7c34913401290dd12b37
wiku <wiku@localhost.localdomain>
2017-09-07 19:05:16 +0200

    Initial commit of empty project

---
+++ src/main/java/com/wiku/rest_client/App.java
@@ -0,0 +1,13 @@
+package com.wiku.rest_client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
